6d558dc380bdaf29c9b65915b9237db682345606
clemensFH <clemens.hoerhan@stud.fh-campuswien.ac.at>
2024-05-29 11:38:08 +0200

    Initial commit

---
+++ app/src/main/java/com/example/books4me/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.books4me
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.sp
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Surface(
+                modifier = Modifier.fillMaxSize(),
+                color = MaterialTheme.colorScheme.background
+            ) {
+                Text(text="Hi", fontSize = 24.sp)
+            }
+        }
+    }
+}
